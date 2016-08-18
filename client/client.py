6edb7cbb9747fb136207c73aeef65c3031e56ee1
Wang Bo <kingbolanda@live.com>
2016-08-18 17:44:46 +0200

    Update client.py

--- client/client.py
+++ client/client.py
@@ -4,7 +4,7 @@ Loop through data in mongodb, tagging images
 from __init__ import *
 
 if __name__ == "__main__":
-    api_url = 'http://139.162.195.12:5000/urbanlearning/api/v1.0/classify/'
+    api_url = 'http://127.0.0.1:5000/urbanlearning/api/v1.0/classify/'
     with Conn(database=conf.MONGO_DB,host=conf.MONGO_IP,port=conf.MONGO_PORT, collection=conf.MONGO_COL) as col:
 	cursor = col.find()
     	for idx,doc in enumerate(cursor):
