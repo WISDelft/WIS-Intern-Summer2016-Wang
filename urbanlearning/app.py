b6771dbddd6da50ebcfea94190776b1fd0b38f08
root <root@ubuntu.members.linode.com>
2016-08-18 09:57:43 +0000

    clean code

--- urbanlearning/app.py
+++ urbanlearning/app.py
@@ -7,18 +7,9 @@ def classification(img_id):
         img = caffe.io.load_image(img_path)
         res_net.blobs['data'].data[...] = transformer.preprocess('data', img)
         out = res_net.forward()
-        top_k = res_net.blobs['prob'].data[0]
-        top_k_label = top_k.flatten().argsort()[-1:-6:-1]
-        top_k_prob = top_k.flatten()[0:5]
-        top_k_prob = [format(i, 'f') for i in top_k_prob]
-        labels = np.loadtxt(mconf.RES_LABEL, str, delimiter='\t')
-        labels = labels[top_k_label].tolist()
-        result = []
-        for idx, label in enumerate(labels):
-            instance = {}
-            instance['label'] = label
-	    instance['prob'] = top_k_prob[idx]
-	    result.append(instance) 
+	top_k_label = util.get_labels(res_net,mconf.RES_LABEL)
+	top_k_prob = util.get_probs(res_net)
+        result = util.get_label_prob_pairs(top_k_label, top_k_prob)
         return jsonify({'status': 200, 'result':result})
     else:
 	return jsonify({'status':400, 'result': 'Resource not found'})
--- urbanlearning/common/util.py
+++ urbanlearning/common/util.py
@@ -35,13 +35,15 @@ def load_image(img_id):
 	doc = col.find_one({'id':img_id})
 	if doc:
 	    link = doc['link']
-	    page_source = requests.get(link)
-	    page_source = html.fromstring(page_source.text)
-	    image_path = page_source.xpath('//meta[@property="og:image"]/@content')[0]
-	    image_path = image_path.split('.jpg')[0] + '.jpg'
-	    file_path = conf.TEMP_PATH + img_id + '.jpg'
-	    print image_path
-	    save_image(image_path, file_path)
+	    try:
+	    	page_source = requests.get(link)
+	    	page_source = html.fromstring(page_source.text)
+	    	image_path = page_source.xpath('//meta[@property="og:image"]/@content')[0]
+	    	image_path = image_path.split('.jpg')[0] + '.jpg'
+	    	file_path = conf.TEMP_PATH + img_id + '.jpg'
+	    	save_image(image_path, file_path)
+	    except IndexError:
+		file_path = None
     return file_path
 
 
@@ -55,6 +57,34 @@ def save_image(url, file_name):
     with open(file_name, 'wb') as out:
 	shutil.copyfileobj(response.raw, out)
     del response
+
+def get_labels(model, label_path):
+    """Get top k label of predicted result
+    """
+    top_k = model.blobs['prob'].data[0]
+    top_k_label = top_k.flatten().argsort()[-1:-6:-1]
+    labels = np.loadtxt(label_path, str, delimiter='\t')
+    labels = labels[top_k_label].tolist()
+    return labels
+
+def get_probs(model):
+    """Get top k prob of predicted result
+    """
+    top_k = model.blobs['prob'].data[0]
+    top_k_prob = top_k.flatten().argsort()[-1:-6:-1]
+    top_k_prob = [format(i, 'f') for i in top_k_prob]
+    return top_k_prob
+
+def get_label_prob_pairs(labels, probs):
+    """Get label probability pair
+    """
+    res = []
+    for idx, label in enumerate(labels):
+	instance = {}
+	instance['label'] = label
+	instance['prob'] = probs[idx]
+        res.append(instance)
+    return res
 
 
 
